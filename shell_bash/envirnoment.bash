0aff1202b93ac22e4bb404459f3bc42b3a87803e
Markus <m4k.join@outlook.com>
2026-03-04 23:01:01 +0100

    get docker ips in linux

--- shell_bash/envirnoment.bash
+++ shell_bash/envirnoment.bash
@@ -1,2 +1,15 @@
 #!/bin/bash
 alias ll='ls -ll'
+
+# add to .bashrc file
+
+function getDocker-IP-Port() {
+        if [ -z $1 ]; then
+                docker ps -a --format "{{.ID}}" | while read -r line ; do
+                        echo $line $(docker inspect --format "{{ .Name }} {{ .NetworkSettings.Networks.bridge.IPAddress }}" $line | sed 's/\///'):$(docker port "$line" | grep -o "0.0.0.0:.*" | cut -f2 -d:)
+                done
+        else
+                echo $(docker inspect --format "{{.ID }} {{ .Name }} {{ .NetworkSettings.Networks.bridge.IPAddress }}" $1 | sed 's/\///'):$(docker port "$1" | grep -o "0.0.0.0:.*" | cut -f2 -d:)
+        fi
+}
+
